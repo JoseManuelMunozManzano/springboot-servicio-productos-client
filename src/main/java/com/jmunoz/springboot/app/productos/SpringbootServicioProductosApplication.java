c3039f9499509509f1cff13edf91fc4c6e126d71
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-17 17:40:41 +0200

    Se anota con @EnableEurekaClient

--- src/main/java/com/jmunoz/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/jmunoz/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,7 +2,12 @@ package com.jmunoz.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+// Con solo informar la dependencia en el pom (eureka-client) de forma automática se configura como cliente.
+// Pero igualmente es buena práctica (no obligatorio) indicarlo con una anotación en la clase principal.
+// Se indica de forma explícita con la anotación @EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
 
